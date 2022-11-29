f0e28572317102246a71a05ce2cbd8e14b894393
indrajadevil001 <91915919+indrajadevil001@users.noreply.github.com>
2022-11-28 21:38:52 -0500

    Update AudioPlayer.java

--- src/main/java/AudioPlayer.java
+++ src/main/java/AudioPlayer.java
@@ -1,16 +1,4 @@
-public class AudioPlayer implements MediaPlayer {
-   MediaAdapter mediaAdapter; 
-
-   @Override
-   public void playMusic(String audioType, String fileName) {		
-
-      if(audioType.equalsIgnoreCase("mp3")){
-         System.out.println("Playing mp3 file " + fileName);			
-      } 
-      
-  
-      else{
-         System.out.println("Audio type not supported for the file" +filename);
-      }
-   }   
+public class AudioPlayer 
+void playVlcPlayer(String filename);
+void playVlcPlayer(String filename);
 }
